1cfa15517fe19487c9549bc6f862545e1afc5550
bariqfirmansyah <bariqsufif@gmail.com>
2021-10-31 12:53:57 +0700

    Add files via upload

---
+++ files/game.js
@@ -0,0 +1,101 @@
+
+var buttonColours = ["red", "blue", "green", "yellow"];
+var highScore = 0;
+var gamePattern = [];
+var userClickedPattern = [];
+
+var started = false;
+var level = 0;
+
+$(".not, .notnot").click(function() {
+  if (!started) {
+    $("#level-title").text("Level " + level);
+    $('.not').animate({opacity:.3});
+    nextSequence();
+    started = true;
+  }
+});
+
+$(".btn").click(function() {
+
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+
+  checkAnswer(userClickedPattern.length-1);
+});
+
+
+function checkAnswer(currentLevel) {
+
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+
+      console.log("success");
+
+      if (userClickedPattern.length === gamePattern.length){
+        setTimeout(function () {
+          nextSequence();
+        }, 1000);
+      }
+
+    } else {
+
+      console.log("wrong");
+
+      playSound("wrong");
+
+      $("body").addClass("game-over");
+      setTimeout(function () {
+        $("body").removeClass("game-over");
+      }, 200);
+
+      if (level-1 > highScore) {
+        highScore = level-1;
+      };
+
+      $("#level-title").text("Game Over, Press 'Play' to restart");
+      $("#Highscore").text("Highscore: "+highScore);
+
+      //2. Call startOver() if the user gets the sequence wrong.
+      startOver();
+    }
+
+}
+
+function nextSequence() {
+
+  userClickedPattern = [];
+  level++;
+  $("#level-title").text("Level " + level);
+
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
+}
+
+function playSound(name) {
+  var audio = new Audio("files/sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
+}
+
+//1. Create a new function called startOver().
+function startOver() {
+
+  //3. Inside this function, you'll need to reset the values of level, gamePattern and started variables.
+  level = 0;
+  gamePattern = [];
+  started = false;
+  $('.not').animate({opacity:1});
+}
